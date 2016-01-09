51397b300af037bacdc494cf7b10b41ae636fa47
phil <phil@dpdk.me>
2016-01-09 14:53:55 +0100

    Another re-factorisation. It's more dynamic and will accept any amount of arguments now.

--- scripts/gif.rb
+++ scripts/gif.rb
@@ -9,50 +9,39 @@ require 'json'
 require 'open-uri'
 
 def grab_random_gif(query)
-  url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=#{query}"  
+  if ARGV.length >= 3
+    query = query[1..-1].join("+")
+  else
+    query = ARGV[0]
+  end
+
+  url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=#{query}"
+  puts url
   source = open(url, &:read)
   parsed_source = JSON.parse(source)
+
   if parsed_source["data"] == []
     puts "No results found."
   else
-    link = parsed_source["data"]["image_original_url"]
-    puts link
+    puts parsed_source["data"]["image_original_url"]
   end
 end
 
 def grab_first_gif(query)
+  if ARGV.length >= 2
+    query = query.join("+")
+  else
+    query = ARGV[0]
+  end
+
   url = "http://api.giphy.com/v1/gifs/search?q=#{query}&api_key=dc6zaTOxFJmzC"
   source = open(url, &:read)
   parsed_source = JSON.parse(source)
-  if parsed_source["data"] == []
-    puts "No results found."
-  else
-    link = parsed_source["data"][0]["images"]["fixed_height"]["url"]
-    puts link
-  end
-end
 
-def grab_first_gif_with_multiple_queries(query1, query2)
-  url = "http://api.giphy.com/v1/gifs/search?q=#{query1}+#{query2}&api_key=dc6zaTOxFJmzC"
-  source = open(url, &:read)
-  parsed_source = JSON.parse(source)
   if parsed_source["data"] == []
     puts "No results found."
   else
-    link = parsed_source["data"][0]["images"]["fixed_height"]["url"]
-    puts link
-  end
-end
-
-def grab_random_gif_with_multiple_queries(query1, query2)
-  url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=#{query1}+#{query2}"
-  source = open(url, &:read)
-  parsed_source = JSON.parse(source)
-  if parsed_source["data"] == []
-    puts "No results found."
-  else
-    link = parsed_source["data"]["image_original_url"]
-    puts link
+    puts parsed_source["data"][0]["images"]["fixed_height"]["url"]
   end
 end
 
@@ -61,14 +50,7 @@ if ARGV[0] == nil
 end
 
 if ARGV[0] == "-r"
-  query = ARGV[1]
-  grab_random_gif(query)
-elsif ARGV.length == 2
-  grab_first_gif_with_multiple_queries(ARGV[0], ARGV[1])
-elsif ARGV.length == 3
-  grab_random_gif_with_multiple_queries(ARGV[1], ARGV[2])
+  grab_random_gif(ARGV)
 else
-  query = ARGV[0]
-  grab_first_gif(query)
+  grab_first_gif(ARGV)
 end
-
